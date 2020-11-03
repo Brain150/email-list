780064ac3f80660f1ee71d6ed578920a4b762b5a
Brain150 <73852713+Brain150@users.noreply.github.com>
2020-11-03 10:36:21 +0100

    Update email_extract.py

--- email_extract.py
+++ email_extract.py
@@ -2,34 +2,35 @@ import re
 
 with open ('mbox-short.txt') as han :
     lines = han.readlines()
-#print(lines)
 
 
-## [.] exact character: hier MOET dus een . staan
-## {m} (cijfer tussen {} zorgt dat de voorgaande RE precies m maal voorkomt (in dit geval: de . mag 1x voorkomen op deze plek ; voorkomt hier mailadresssen zonder volledig domein 
-emails = re.findall("[\w\.-]+@[\w\.-]+[.]{1}[\w\.-]+", str(lines)) ## geeft list met alles erin wat x@x.x bevat (ook als er na de @ meer .x.x.x is!) en heeft \n, <,> erafgehaald ; str omdat RE een verzameling STRINGS is
+# [.] exact character: hier MOET dus een . staan
+# {m} (cijfer tussen {} zorgt dat de voorgaande RE precies m maal voorkomt (in dit geval: de . mag 1x voorkomen op deze plek ; voorkomt hier mailadressen zonder volledig domein 
+# creates list with items containing x@x.x (with unlimited @x.x.x), remoes \n, <,>, etc.
+# str() because RE is a collection of strings
+
+emails = re.findall("[\w\.-]+@[\w\.-]+[.]{1}[\w\.-]+", str(lines)) 
 #print(emails)
 
-## check & remove duplicates in list 'emails'
-## list(dict.fromkeys(emails)) maakt een dictionary van alleen keys, geen values: unieke keys, want dictionary kan geen duplicaten hebben; verandert de dictionary weer naar list
-## gebruik gemaakt van een functie met return, zodat je nog meer kunt met het resultaat
+# check & remove duplicates in list 'emails'
+# list(dict.fromkeys(emails)) generates a dictionary containing unique keys only (no values): dictionarie does not supoprt duplicates; changes dictionary back into list
+# finction with a return defined and called: not really necessary, but it looks neater (other advantages?)
 
 def single(emails):
   return list(dict.fromkeys(emails))
 
-unique = single(emails) ## call function
+unique = single(emails) # call function
 
-## unieke emails naar nieuwe file schrijven
+# wirte unique emails adresses to new file (automatically generated
 with open ("email.txt", "w" ) as file1 :
     for line in unique :
-        file1.write(line + "\n") ## \n zodat elk item op een nieuwe regel geschreven wordt
+        file1.write(line + "\n") # \n to write eacht item on a separate line
 
 
 '''
-## andere schrijfopdracht (nog niet getest)
-
+## alternative for writing (not tested)
 new_file = open ("newfile.txt", mode = "a+", encoding = "utf-8")
-new_file.writelines(email)
+new_file.writelines(email, "\n")
 for line in new_file :
     print(line)
 new_file.close()
