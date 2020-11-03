c706abd81e43f1f585e9dce419ae1f07b22bb68e
Brain150 <73852713+Brain150@users.noreply.github.com>
2020-11-03 10:37:25 +0100

    Update email_extract.py

--- email_extract.py
+++ email_extract.py
@@ -3,25 +3,21 @@ import re
 with open ('mbox-short.txt') as han :
     lines = han.readlines()
 
-
 # [.] exact character: hier MOET dus een . staan
 # {m} (cijfer tussen {} zorgt dat de voorgaande RE precies m maal voorkomt (in dit geval: de . mag 1x voorkomen op deze plek ; voorkomt hier mailadressen zonder volledig domein 
 # creates list with items containing x@x.x (with unlimited @x.x.x), remoes \n, <,>, etc.
 # str() because RE is a collection of strings
-
 emails = re.findall("[\w\.-]+@[\w\.-]+[.]{1}[\w\.-]+", str(lines)) 
 #print(emails)
 
 # check & remove duplicates in list 'emails'
 # list(dict.fromkeys(emails)) generates a dictionary containing unique keys only (no values): dictionarie does not supoprt duplicates; changes dictionary back into list
 # finction with a return defined and called: not really necessary, but it looks neater (other advantages?)
-
 def single(emails):
   return list(dict.fromkeys(emails))
-
 unique = single(emails) # call function
 
-# wirte unique emails adresses to new file (automatically generated
+# write unique emails adresses to new file (automatically generated
 with open ("email.txt", "w" ) as file1 :
     for line in unique :
         file1.write(line + "\n") # \n to write eacht item on a separate line
